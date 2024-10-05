7fceb64fcb8256f58cf034fec6a7bef5e9785542
lloyddraysey@gmail.com <lloyddraysey@gmail.com>
2024-10-05 10:24:42 +0000

    Handling to throw error if function returns bad output

--- tests/test_hello_message.cpp
+++ tests/test_hello_message.cpp
@@ -6,5 +6,9 @@
 int main(){
     EVP_PKEY * pubKey = Client_Key_Gen::loadPublicKey("public_key.pem");
     std::string helloMessage = HelloMessage::generateHelloMessage(pubKey);
+    if (helloMessage == ""){
+        return -1;
+    }
     std::cout << helloMessage << std::endl;    
+    return 0;
 }
