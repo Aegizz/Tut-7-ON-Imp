4c093c4d40f86e7c196ea8ea675b74ecf9228d37
gradyclark03 <80494598+gradyclark03@users.noreply.github.com>
2024-10-19 00:48:22 +1030

    Fixed hello message

--- tests/test_hello_message.cpp
+++ tests/test_hello_message.cpp
@@ -4,7 +4,17 @@
 
 
 int main(){
+    if(Client_Key_Gen::key_gen(0, false, true)){
+        std::cerr << "Key generation failed!" << std::endl;
+        return 1;
+    }
+
     EVP_PKEY * pubKey = Client_Key_Gen::loadPublicKey("tests/test-keys/public_key0.pem");
+    if(!pubKey){
+        std::cerr << "Failed to load public key!" << std::endl;
+        return 1;
+    }
+
     std::string helloMessage = HelloMessage::generateHelloMessage(pubKey);
     if (helloMessage == ""){
         return -1;
